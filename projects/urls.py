0b10f284275895220139f42a2e89c4b7cc46ceb2
itzik <itzikd15@gmail.com>
2021-08-29 17:20:22 +0300

    try to add projects api - doesnt work

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
-
-app_name = 'projects'
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="projects/index.html")),
+    path('', views.ProjectsSerializer.as_view()),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from projects.models import Projects
+from rest_framework import generics
+from .serializers import ProjectsSerializer
 
-# Create your views here.
+
+# Lead Viewset
+class ProjectsListCreate(generics.ListCreateAPIView):
+    queryset = Projects.objects.all()
+    serializer_class = ProjectsSerializer
